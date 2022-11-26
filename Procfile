f130681e1793890804559e53bcafff3d947bef50
Qalbun Salim <qalbunsb@gmail.com>
2022-11-26 19:50:58 +0700

    configure deployment settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'gorkuy-backend.wsgi'
--- gorkuy_backend/settings.py
+++ gorkuy_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=pn2&3#bwh=63z1si9gkcmncaltdoh#)*mc2yuon&wjynz#w@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
