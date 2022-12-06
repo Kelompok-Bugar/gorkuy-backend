d61764d6bbde380f440bf02bb720294eca422a9d
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2022-12-06 14:33:52 +0700

    prevent cretae new records

--- edit_ketersediaan/views.py
+++ edit_ketersediaan/views.py
@@ -4,6 +4,7 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 import datetime as dt
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 ## Return the designated lapangan info
 def show_edit_form(request, id):
@@ -24,20 +25,18 @@ def show_edit_form(request, id):
 @csrf_exempt
 def edit_jadwal(request, id):
     if request.method == 'POST':
-        print('oke')
         edited_lapangan = Lapangan.objects.get(pk=id)
 
         ## Get the jadwal from string and convert it to datetime.time
         jam_buka = request.POST.get("jam_buka")
         jam_tutup = request.POST.get("jam_tutup")
-        
         time_buka_hr =  jam_buka.split(':')[0]
         time_tutup_hr = jam_tutup.split(':')[0]
 
         edited_lapangan.jam_buka = dt.time(int(time_buka_hr),00)
         edited_lapangan.jam_tutup = dt.time(int(time_tutup_hr),00)
 
-        edited_lapangan.save(['jam_buka','jam_tutup'])
+        edited_lapangan.save()
 
     return HttpResponse(status=201)
     
