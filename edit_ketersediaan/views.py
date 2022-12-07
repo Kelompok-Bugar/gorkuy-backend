9343de6237c12d376695bbe93ec2d610c77c6a90
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2022-12-07 22:57:41 +0700

    validating function

--- edit_ketersediaan/views.py
+++ edit_ketersediaan/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from database.models import Jadwal, Lapangan
-from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http.response import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 import datetime as dt
 from django.views.decorators.csrf import csrf_exempt
@@ -33,6 +33,13 @@ def edit_jadwal(request, id):
         time_buka_hr =  jam_buka.split(':')[0]
         time_tutup_hr = jam_tutup.split(':')[0]
 
+        ## Validate changes
+        if not validate_change(edited_lapangan, time_buka_hr, time_tutup_hr) :
+            return JsonResponse({
+                'status':'failed',
+                'message':'Tidak bisa mengedit ketersediaan lapangan karena ada jadwal yang bentrok'},
+                 status=400)
+                 
         edited_lapangan.jam_buka = dt.time(int(time_buka_hr),00)
         edited_lapangan.jam_tutup = dt.time(int(time_tutup_hr),00)
 
@@ -40,3 +47,19 @@ def edit_jadwal(request, id):
 
     return HttpResponse(status=201)
     
+## Validate request if there's conflict between booked jadwal
+## and new jadwal
+def validate_change(lapangan, jam_buka, jam_tutup):
+    jadwals = lapangan.jadwal_set.all().order_by('start').values()
+
+    first_jadwal = jadwals[0]
+    last_jadwal = jadwals[len(jadwals)-1]
+
+    time_buka = dt.time(int(jam_buka),00)
+    time_tutup = dt.time(int(jam_tutup),00)
+
+    ## first jadwal can't be before open time and last jadwal can't be after close time
+    if first_jadwal['start'] < time_buka or last_jadwal['end'] > time_tutup:
+        return False 
+
+    return True
