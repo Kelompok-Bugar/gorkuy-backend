9fcc78d33fc224ddc785173e9ac9a6b10f1d3e8a
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2022-12-10 20:28:26 +0700

    frontend integration

--- edit_ketersediaan/urls.py
+++ edit_ketersediaan/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('<int:id>', show_edit_form, name='show_edit_form'),
     path('save/<int:id>', edit_jadwal, name='edit_jadwal'),   
+    path('myLapangan',show_my_lapangan,name='show_my_lapangan')
 ]
--- edit_ketersediaan/views.py
+++ edit_ketersediaan/views.py
@@ -5,21 +5,32 @@ from django.core import serializers
 import datetime as dt
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.contrib import messages
 
 ## Return the designated lapangan info
 def show_edit_form(request, id):
-    lapangan = Lapangan.objects.filter(pk=id)
+    lapangan = Lapangan.objects.get(pk=id)
+
+    ## Give hour choice and jam_buka jam_tutup lapangan 
+    hour_choices = [{
+        'values': '{:02d}:00:00'.format(x),
+        'show':'{:02d}:00'.format(x),
+        'jam_buka_selected': True if '{:02d}:00:00'.format(x) == str(lapangan.jam_buka) else False,
+        'jam_tutup_selected': True if '{:02d}:00:00'.format(x) == str(lapangan.jam_tutup) else False
+    }
+    for x in range(0, 24)]
 
     context = {
-        'lapangan':lapangan 
+        'lapangan':lapangan,
+        'hour_choices':hour_choices
     }
 
     # The function will give the form the data needed to the update_form
-    # return render(request,'update_form.html',context)
+    return render(request,'update_form.html',context)
 
     # For now the function will return the lapangan object as JSON
     # print(Jadwal.objects.filter(lapangan_id = id))
-    return HttpResponse(serializers.serialize("json", lapangan), content_type="application/json")
+    # return HttpResponse(serializers.serialize("json", lapangan), content_type="application/json")
 
 ## Save the edited jadwal
 @csrf_exempt
@@ -28,8 +39,8 @@ def edit_jadwal(request, id):
         edited_lapangan = Lapangan.objects.get(pk=id)
 
         ## Get the jadwal from string and convert it to datetime.time
-        jam_buka = request.POST.get("jam_buka")
-        jam_tutup = request.POST.get("jam_tutup")
+        jam_buka = request.POST.get("jam-buka")
+        jam_tutup = request.POST.get("jam-tutup")
         time_buka_hr =  jam_buka.split(':')[0]
         time_tutup_hr = jam_tutup.split(':')[0]
 
@@ -37,15 +48,17 @@ def edit_jadwal(request, id):
         if not validate_change(edited_lapangan, time_buka_hr, time_tutup_hr) :
             return JsonResponse({
                 'status':'failed',
-                'message':'Tidak bisa mengedit ketersediaan lapangan karena ada jadwal yang bentrok'},
-                 status=400)
+                'message':'Tidak bisa mengedit ketersediaan lapangan karena ada jadwal yang bentrok'})
                  
         edited_lapangan.jam_buka = dt.time(int(time_buka_hr),00)
         edited_lapangan.jam_tutup = dt.time(int(time_tutup_hr),00)
 
         edited_lapangan.save()
 
-    return HttpResponse(status=201)
+    return JsonResponse({
+                'status':'Success',
+                'message':'Jadwal Berhasil diperbarui'},
+                 status=201)
     
 ## Validate request if there's conflict between booked jadwal
 ## and new jadwal
@@ -63,3 +76,13 @@ def validate_change(lapangan, jam_buka, jam_tutup):
         return False 
 
     return True
+
+def show_my_lapangan(request):
+    mitra = request.user
+
+    lapangan = Lapangan.objects.filter(mitra=mitra)
+    context = {
+        'lapangans':lapangan,
+    }
+    
+    return render(request, 'lapangan_page.html',context)
